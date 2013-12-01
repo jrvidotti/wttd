4dfb7029a4860057f25d425452b961d34a363b91
Junior Vidotti <jrvidotti@gmail.com>
2013-12-01 20:50:26 -0300

    adicionado django-decouple

--- eventex/settings.py
+++ eventex/settings.py
@@ -10,22 +10,24 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
-import dj_database_url
+from decouple import config
+from dj_database_url import parse as db_url
 from unipath import Path
+
 BASE_DIR = Path(__file__).parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(b2m*ke0l@6v67vx-+0=d+7p9mqwsyo2vu+!+_yt3iwdp6cx#o'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -38,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eventex.core',
+#    'eventex.subscriptions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +61,10 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default = 'sqlite:///' + BASE_DIR.child('db.sqlite3')),
+    'default': config(
+        'DATABASE_URL',
+        default = 'sqlite:///' + BASE_DIR.child('db.sqlite3'),
+        cast=db_url),
 }
 
 # Internationalization
